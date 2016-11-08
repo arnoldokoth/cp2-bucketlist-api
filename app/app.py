c0798a3785974cdcbfb945a125d1273e42dd4235
Arnold Okoth <arnold.okoth@andela.com>
2016-11-08 09:35:58 +0300

    User registration, login & token authentication ref:#8 #11 #12

--- app/app.py
+++ app/app.py
@@ -1,17 +1,36 @@
 import os
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature, SignatureExpired)
 
-from flask import Flask, jsonify, abort, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth
-from model_interactions import *
-auth = HTTPBasicAuth()
+from flask import Flask, jsonify, abort, request, g
+from flask_httpauth import HTTPTokenAuth
+from sqlalchemy.orm import sessionmaker
+from models.models import Base, engine, User
+
+
+auth = HTTPTokenAuth(scheme='Token')
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
-                                                                    'blapi.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = '\x19\xffDM\xbd\x12\x02\xf1\x90ZR\x16`\xfc\x13\xa7^7b\\\x8d5%\x12'
-db = SQLAlchemy(app)
+app.config[
+    'SECRET_KEY'] = '\x19\xffDM\xbd\x12\x02\xf1\x90ZR\x16`\xfc\x13\xa7^7b\\\x8d5%\x12'
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@auth.verify_token
+def verify_auth_token(token):
+    s = Serializer(app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except SignatureExpired:
+        # The token is valid but has expired
+        return None
+    except BadSignature:
+        # The token is invalid
+        return None
+    user_id = data['id']
+    return user_id
 
 
 @app.errorhandler(404)
@@ -19,20 +38,59 @@ def bucketlist_not_found(error):
     return jsonify({'message': 'Bucket List Not Found'})
 
 
+def verify_password(username, password):
+    user = session.query(User).filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.current_user = user
+    return True
+
+
 @app.route('/auth/register', methods=['POST'])
 def register_new_user():
-    return jsonify({'message': 'I got the request'})
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Check if username | password are provided
+    if username is None or password is None:
+        return jsonify({'message': 'Username/Password Not Provided!'})
+
+    # Check if the username already exists
+    if session.query(User).filter_by(username=username).first() is not None:
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message': 'User already exists!'})
+        # Status Code: 201 -> Test This
+
+    user = User(username=username)
+    user.hash_password(password)
+    session.add(user)
+
+    try:
+        session.commit()
+    except Exception:
+        session.rollback()
+        return jsonify({'message': 'Error occured while adding user!'})
+    return jsonify({'user': user.username})
+    # Status Code: 200 -> Test This
 
 
 @app.route('/auth/login', methods=['POST'])
 def login_user():
-    username = request.json['username']
-    return jsonify({'message': 'Hello {0}'.format(username)})
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if verify_password(username, password):
+        token = g.current_user.generate_auth_token()
+        return jsonify({
+            'message': 'Hello, {0}'.format(g.current_user.username),
+            'token': token.decode('ascii')
+            })
+    else:
+        return jsonify({'message': 'Invalid Username/Password'})
 
 
 @app.route('/bucketlists', methods=['POST'])
 @auth.login_required
-def create_bucket_list(self):
+def create_bucket_list():
     return jsonify({'message': 'Creating Bucket List'})
 
 
