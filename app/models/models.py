e353c43742d33a2768996bbdfba18e8653d787a4
Arnold Okoth <arnold.okoth@andela.com>
2016-11-08 10:44:43 +0300

    Remove use of id (id -> 'tablename'_id)

--- app/models/models.py
+++ app/models/models.py
@@ -1,13 +1,10 @@
-import random
-import string
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.apps import custom_app_context as pwd_context
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
-                          BadSignature, SignatureExpired)
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 Base = declarative_base()
 secret_key = '\x19\xffDM\xbd\x12\x02\xf1\x90ZR\x16`\xfc\x13\xa7^7b\\\x8d5%\x12'
@@ -15,7 +12,7 @@ secret_key = '\x19\xffDM\xbd\x12\x02\xf1\x90ZR\x16`\xfc\x13\xa7^7b\\\x8d5%\x12'
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     username = Column(String(40), unique=True)
     password_hash = Column(String(64))
 
@@ -29,22 +26,22 @@ class User(Base):
 
     def generate_auth_token(self, expiration=300):
         s = Serializer(secret_key, expires_in=expiration)
-        return s.dumps({'id': self.id})
+        return s.dumps({'id': self.user_id})
 
 
 class BucketList(Base):
     __tablename__ = 'bucketlist'
-    id = Column(Integer, primary_key=True)
+    bucketlist_id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     date_created = Column(DateTime, default=datetime.utcnow)
     date_modified = Column(DateTime, default=datetime.utcnow)
-    created_by = Column(Integer, ForeignKey('user.id'))
+    created_by = Column(Integer, ForeignKey('user.user_id'))
 
 
 class BucketListItems(Base):
     __tablename__ = 'bucketlistitems'
-    id = Column(Integer, primary_key=True)
-    bucketlist_id = Column(Integer, ForeignKey('bucketlist.id'))
+    item_id = Column(Integer, primary_key=True)
+    bucketlist_id = Column(Integer, ForeignKey('bucketlist.bucketlist_id'))
     name = Column(String(100), nullable=False)
     date_created = Column(DateTime, default=datetime.utcnow)
     date_modified = Column(DateTime, default=datetime.utcnow)
