213f8b0fef525aaf61e2b050681afdaf291cd13a
Arnold Okoth <arnold.okoth@andela.com>
2016-11-16 19:28:51 +0300

    Update test_setup.py ref: #23

--- app/tests/test_setup.py
+++ app/tests/test_setup.py
@@ -1,17 +1,28 @@
+import json
 import unittest
-
-# from app.models.models import User, BucketList, BucketListItems
-from app.app import db, app
+from app.app import app, db
 
 
 class TestSetup(unittest.TestCase):
 
     def setUp(self):
+        # Create Test User
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tests/tests.db'
-        # Create a Test Client
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
         self.app = app.test_client()
         db.create_all()
+        credentials = {'username': 'testuser', 'password': 'testpassword'}
+        self.app.post('/auth/register', data=json.dumps(credentials),
+                      content_type='application/json')
+
+        response = self.app.post('/auth/login',
+                                 data=json.dumps(credentials),
+                                 content_type='application/json')
+        token = json.loads(response.get_data().decode())['token']
+        self.headers = {
+            'Authorization': 'Token ' + token
+        }
 
     def tearDown(self):
         db.session.remove()
